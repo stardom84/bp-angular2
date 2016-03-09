a66b2979efb1ae77fb1df3025b59a5e7c5d575df
KYOUNGTAE KANG <kyoungtaek@allianz.com>
2016-03-09 17:56:28 +0700

    change gulpfile.js to gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,7 +1,14 @@
-var gulp = require('gulp'), plugins = require('gulp-load-plugins')();
-var path = require('path'), fork = require('child_process');
+var gulp = require('gulp'),
+	plugins = require('gulp-load-plugins')();
+
+var path = require('path'),
+	fork = require('child_process');
+
 fork = fork.fork;
+
 var pathExists = require('path-exists');
+
+
 process.env.NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : 'development';
 process.env.PORT = process.env.PORT ? process.env.PORT : '8080';
 var env = {
@@ -17,15 +24,20 @@ var env = {
 		return; //this.isDev ? paths.dev : paths.prod;
 	}
 };
+
+
 /**
  * Definitions
  */
+
 var files = [
 	'typings/main.d.ts',
 	'src/**/*.ts'
 ], changed;
-function ts(filesRoot, filesDest, tsProject, base) {
+
+function ts(filesRoot:String, filesDest:String, tsProject:any, base?:String) {
 	files = changed ? changed : files;
+
 	var result = gulp.src(['typings/main.d.ts'].concat(files), {base: path.join(filesRoot, base ? base : '')})
 		.pipe(plugins.debug({title: 'Stream contents:', minimal: true}))
 		.pipe(plugins.tslint())
@@ -39,44 +51,66 @@ function ts(filesRoot, filesDest, tsProject, base) {
 		})))
 		.pipe(gulp.dest(filesDest));
 }
+
 var filesRoot = 'src';
 var filesDest = 'build';
+
 function tsSrc() {
+
+
 	var tsProject = plugins.typescript.createProject('tsconfig.json');
+
 	return ts(filesRoot, filesDest, tsProject);
 }
+
 function tsServer() {
+
 	var base = 'server';
 	var tsProject = plugins.typescript.createProject('tsconfig.json');
+
 	return ts(filesRoot, filesDest, tsProject, base);
 }
+
 function tsApp() {
+
 	var tsProject = plugins.typescript.createProject('tsconfig.json');
+
 	return ts(filesRoot, filesDest, tsProject);
 }
+
 function casper() {
 	return gulp.src('build/server.js', {read: false})
 		.pipe(plugins.shell([
 			'node <%= file.path %>'
 		]));
 }
+
 function mongodb() {
 	return gulp.src(['D:/workspace/mongodb'])
 		.pipe(plugins.shell(['mongod --dbpath <%= file.path %>']));
 }
+
 function watch() {
 	var watch = gulp.watch('src/**/*.ts', tsSrc);
-	watch.on('change', function (path) {
+
+	watch.on('change', function (path:String) {
 		changed = path;
 	});
 }
+
 /**
  * Public Tasks
  */
-gulp.task('build', gulp.series(tsSrc, watch));
+
+gulp.task('build', gulp.series(
+	tsSrc, watch
+));
 gulp.task('build:server', gulp.series(tsServer));
 gulp.task('casper', gulp.series(casper));
+
 gulp.task('mongodb', gulp.series(mongodb));
 /**
  * Watches
  */
+
+
---
+++ src/server/server.ts
@@ -0,0 +1,23 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as favicon from 'serve-favicon';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+var port:number = process.env.PORT || 3000,
+	app = express();
+
+app.use('/app', express.static(path.resolve(__dirname, 'app')));
+app.use('/libs', express.static(path.resolve(__dirname, 'libs')));
+
+var renderIndex = (req:express.Request, res:express.Response) => {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
+};
+
+app.get('/*', renderIndex);
+
+var server = app.listen(port, ()=> {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('This express app is listening on port:' + port);
+});
